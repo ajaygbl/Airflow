afd301469c1e6d8822c6e5acfcfaa5edeb0c7dfb
ajaygbl <ajaygbl@gmail.com>
2025-11-09 08:58:01 +0530

    added xcom age and commented op_kwargs

--- airflow_docker/dags/with_python_operator.py
+++ airflow_docker/dags/with_python_operator.py
@@ -10,17 +10,20 @@ default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=5),
 }
+def get_age(ti):
+     ti.xcom_push(key='age', value=35)
 
 def get_name(ti):
     ti.xcom_push(key='firstname', value='Airflow')
     ti.xcom_push(key='lastname', value='User')
 
-def print_hello(age, ti):
+def print_hello(ti):
     firstname = ti.xcom_pull(task_ids='get_name', key='firstname')
     lastname = ti.xcom_pull(task_ids='get_name', key='lastname')
+    age = ti.xcom_pull(task_ids='age_task', key='age')
     print(f"Hello world from PythonOperator name: {firstname} {lastname}, age: {age}!")
 
-with DAG(dag_id="first_python_dag_v4", 
+with DAG(dag_id="first_python_dag_v5", 
          default_args=default_args,
          start_date=datetime(2025, 10, 12, 1,1,1),
          schedule="@daily", 
@@ -31,9 +34,14 @@ with DAG(dag_id="first_python_dag_v4",
         )
         task1 = PythonOperator(
             task_id='print_hello',
-            python_callable=print_hello,
-            op_kwargs={'age': 5}
+            python_callable=print_hello
+            #op_kwargs={'age': 5}
         )      
 
+        task3 = PythonOperator(
+            task_id='age_task',
+            python_callable=get_age
+        )
 
-        task2 >> task1
+
+        [task2, task3] >> task1
